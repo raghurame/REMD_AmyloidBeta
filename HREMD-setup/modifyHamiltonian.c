629720ecdd218a16829310441b2893cda7e304d3
Raghuram <83778193+raghurame@users.noreply.github.com>
2022-08-28 23:41:43 +0530

    daily updates

---
+++ HREMD-setup/modifyHamiltonian.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+/*
+Segments in topol.top
+
+[ moleculetype ]
+[ atoms ]
+[ bonds ]
+[ pairs ]
+[ angles ]
+[ dihedrals ]
+[ position_restraints ]
+[ system ]
+[ molecules ]
+[ system ]
+*/
+
+int main(int argc, char const *argv[])
+{
+	FILE *ffBondedITP, *ffNonbondedITP, *topolTopITP;
+	ffBondedITP = fopen ("ffbonded.itp");
+	ffNonbondedITP = fopen ("ffnonbonded.itp");
+	topolTopITP = fopen ("topol.top");
+
+
+
+	fclose (ffBondedITP);
+	fclose (ffNonbondedITP);
+	fclose (topolTopITP);
+	return 0;
+}
