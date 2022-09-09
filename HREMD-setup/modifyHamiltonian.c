98dd123159d3a84ac864aa60efcea077baf72b8f
Raghuram Elancheran <83778193+raghurame@users.noreply.github.com>
2022-09-09 23:13:55 +0530

    daily updates

--- HREMD-setup/modifyHamiltonian.c
+++ HREMD-setup/modifyHamiltonian.c
@@ -74,7 +74,7 @@ typedef struct molecule
 
 typedef struct bondedDefines
 {
-	char defineString, defineType;
+	char defineString[10], defineType[10];
 	float value, constant;
 	int np;
 } BONDED_DEFINES;
@@ -93,7 +93,7 @@ typedef struct nonbondedParams
 	float c6, c12;
 } NONBONDED_PARAMS;
 
-TOPOLOGY_ATOMS *readTopAtoms (FILE *topolTopITP, TOPOLOGY_BOOL topCurrentPosition, TOPOLOGY_ATOMS *inputAtoms, int *nAtoms)
+TOPOLOGY_ATOMS *readTopAtoms (FILE *topolTopITP, TOPOLOGY_BOOL topCurrentPosition, TOPOLOGY_ATOMS *inputAtoms, int *nAtoms, FILE *topolTopITP_output, float lambda)
 {
 	rewind (topolTopITP);
 	char lineString[2000], atomString[2000];
@@ -128,20 +128,25 @@ TOPOLOGY_ATOMS *readTopAtoms (FILE *topolTopITP, TOPOLOGY_BOOL topCurrentPositio
 	// Resetting the position bool
 	topCurrentPosition.atoms = 0;
 
-	// One empty line was also counted in the previous loop
-	printf("Number of atoms detected in topology file: %d\n", (*nAtoms) - 1);
+	// To compensate for the extra empty line counted in the first 'while' loop
+	(*nAtoms) -= 1;
 
-	inputAtoms = (TOPOLOGY_ATOMS *) malloc ((*nAtoms) * sizeof (TOPOLOGY_ATOMS));
+	// One empty line was also counted in the previous loop
+	printf("Number of atoms detected in topology file: %d\n", (*nAtoms));
+
+	inputAtoms = (TOPOLOGY_ATOMS *) malloc ((*nAtoms + 1) * sizeof (TOPOLOGY_ATOMS));
 
 	rewind (topolTopITP);
 	int currentAtom = 0;
+
 	// Storing the information under [ atoms ] directive
 	while (fgets (lineString, 2000, topolTopITP) != NULL)
 	{
 		if (lineString[0] != ';')
 		{
 			if ((topCurrentPosition.atoms == 1) && (lineString[0] == '[')) {
-				topCurrentPosition.atoms = 0; }
+				topCurrentPosition.atoms = 0;
+				fprintf(topolTopITP_output, "\n"); }
 
 			if (topCurrentPosition.atoms == 1)
 			{
@@ -151,20 +156,23 @@ TOPOLOGY_ATOMS *readTopAtoms (FILE *topolTopITP, TOPOLOGY_BOOL topCurrentPositio
 
 				sscanf (atomString, "%d\n", &currentAtom);
 				sscanf (atomString, "%d %s %d %s %s %d %f %f", &inputAtoms[currentAtom - 1].nr, &inputAtoms[currentAtom - 1].type, &inputAtoms[currentAtom - 1].resnr, &inputAtoms[currentAtom - 1].residue, &inputAtoms[currentAtom - 1].atom, &inputAtoms[currentAtom - 1].cgnr, &inputAtoms[currentAtom - 1].charge, &inputAtoms[currentAtom - 1].mass);
+
+				fprintf (topolTopITP_output, "%d\t\t%s\t\t%d\t\t%s\t\t%s\t\t%d\t\t%f\t\t%f\n", inputAtoms[currentAtom - 1].nr, inputAtoms[currentAtom - 1].type, inputAtoms[currentAtom - 1].resnr, inputAtoms[currentAtom - 1].residue, inputAtoms[currentAtom - 1].atom, inputAtoms[currentAtom - 1].cgnr, inputAtoms[currentAtom - 1].charge * lambda, inputAtoms[currentAtom - 1].mass);
 			}
 
 			if (strstr (lineString, "[ atoms ]")) {
+				fprintf(topolTopITP_output, "%s", lineString);
 				topCurrentPosition.atoms = 1; }
+
+			if (topCurrentPosition.atoms == 0) {
+				fprintf(topolTopITP_output, "%s", lineString); }
 		}
 	}
-
-	// To compensate for the extra empty line counted in the first 'while' loop
-	(*nAtoms) -= 1;
 
 	return inputAtoms;
 }
 
-void readBondedITP (FILE *ffBondedITP, TOPOLOGY_BOOL topCurrentPosition, BONDED_DEFINES **inputBondDefines, BONDED_DEFINES **inputAngleDefines, BONDED_DEFINES **inputProperDihedralDefines, BONDED_DEFINES **inputImproperDihedralDefines, int *nBondDefines, int *nAngleDefines, int *nProperDihedralDefines, int *nImproperDihedralDefines)
+void readBondedITP (FILE *ffBondedITP, TOPOLOGY_BOOL topCurrentPosition, BONDED_DEFINES **inputBondDefines, BONDED_DEFINES **inputAngleDefines, BONDED_DEFINES **inputProperDihedralDefines, BONDED_DEFINES **inputImproperDihedralDefines, int *nBondDefines, int *nAngleDefines, int *nProperDihedralDefines, int *nImproperDihedralDefines, FILE *ffBondedITP_output, float lambda)
 {
 	rewind (ffBondedITP);
 	char lineString[2000];
@@ -204,31 +212,79 @@ void readBondedITP (FILE *ffBondedITP, TOPOLOGY_BOOL topCurrentPosition, BONDED_
 	rewind (ffBondedITP);
 
 	int currentBondDefine = 0, currentAngleDefine = 0, currentProperDihedralDefine = 0, currentImproperDihedralDefine = 0;
+	char *defineType_local;
+	defineType_local = (char *) malloc (500 * sizeof (char));
 
 	while (fgets (lineString, 2000, ffBondedITP) != NULL)
 	{
 		if (lineString[0] != ';' && lineString[0] == '#')
 		{
 			if (strstr (lineString, "gb_")) {
-				sscanf (lineString, "%s %s %f %f\n", &(*inputBondDefines)[currentBondDefine].defineString, &(*inputBondDefines)[currentBondDefine].defineType, &(*inputBondDefines)[currentBondDefine].value, &(*inputBondDefines)[currentBondDefine].constant);
+				sscanf (lineString, "%s %s %f %f\n", 
+					&(*inputBondDefines)[currentBondDefine].defineString,
+					&(*inputBondDefines)[currentBondDefine].defineType, 
+					&(*inputBondDefines)[currentBondDefine].value, 
+					&(*inputBondDefines)[currentBondDefine].constant);
+
+				fprintf(ffBondedITP_output, "%s\t%s\t%f\t%f\n", 
+					(*inputBondDefines)[currentBondDefine].defineString,
+					(*inputBondDefines)[currentBondDefine].defineType, 
+					(*inputBondDefines)[currentBondDefine].value, 
+					(*inputBondDefines)[currentBondDefine].constant * lambda);
+
 				currentBondDefine++; }
 
 			if (strstr (lineString, "ga_")) {
-				sscanf (lineString, "%s %s %f %f\n", &(*inputAngleDefines)[currentAngleDefine].defineString, &(*inputAngleDefines)[currentAngleDefine].defineType, &(*inputAngleDefines)[currentAngleDefine].value, &(*inputAngleDefines)[currentAngleDefine].constant);
+				sscanf (lineString, "%s %s %f %f\n", 
+					&(*inputAngleDefines)[currentAngleDefine].defineString, 
+					&(*inputAngleDefines)[currentAngleDefine].defineType, 
+					&(*inputAngleDefines)[currentAngleDefine].value, 
+					&(*inputAngleDefines)[currentAngleDefine].constant);
+
+				fprintf(ffBondedITP_output, "%s\t%s\t%f\t%f\n", 
+					(*inputAngleDefines)[currentAngleDefine].defineString, 
+					(*inputAngleDefines)[currentAngleDefine].defineType, 
+					(*inputAngleDefines)[currentAngleDefine].value, 
+					(*inputAngleDefines)[currentAngleDefine].constant * lambda);
+
 				currentAngleDefine++; }
 
 			if (strstr (lineString, "gi_")) {
-				sscanf (lineString, "%s %s %f %f\n", &(*inputImproperDihedralDefines)[currentImproperDihedralDefine].defineString, &(*inputImproperDihedralDefines)[currentImproperDihedralDefine].defineType, &(*inputImproperDihedralDefines)[currentImproperDihedralDefine].value, &(*inputImproperDihedralDefines)[currentImproperDihedralDefine].constant);
+				sscanf (lineString, "%s %s %f %f\n", 
+					&(*inputImproperDihedralDefines)[currentImproperDihedralDefine].defineString, 
+					&(*inputImproperDihedralDefines)[currentImproperDihedralDefine].defineType, 
+					&(*inputImproperDihedralDefines)[currentImproperDihedralDefine].value, 
+					&(*inputImproperDihedralDefines)[currentImproperDihedralDefine].constant);
+
+				fprintf(ffBondedITP_output, "%s\t%s\t%f\t%f\n", 
+					(*inputImproperDihedralDefines)[currentImproperDihedralDefine].defineString, 
+					(*inputImproperDihedralDefines)[currentImproperDihedralDefine].defineType, 
+					(*inputImproperDihedralDefines)[currentImproperDihedralDefine].value, 
+					(*inputImproperDihedralDefines)[currentImproperDihedralDefine].constant * lambda);
+
 				currentImproperDihedralDefine++; }
 
 			if (strstr (lineString, "gd_")) {
-				sscanf (lineString, "%s %s %f %f %d\n", &(*inputProperDihedralDefines)[currentProperDihedralDefine].defineString, &(*inputProperDihedralDefines)[currentProperDihedralDefine].defineType, &(*inputProperDihedralDefines)[currentProperDihedralDefine].value, &(*inputProperDihedralDefines)[currentProperDihedralDefine].constant, &(*inputProperDihedralDefines)[currentProperDihedralDefine].np);
+				sscanf (lineString, "%s %s %f %f %d\n", 
+					&(*inputProperDihedralDefines)[currentProperDihedralDefine].defineString, 
+					&(*inputProperDihedralDefines)[currentProperDihedralDefine].defineType, 
+					&(*inputProperDihedralDefines)[currentProperDihedralDefine].value, 
+					&(*inputProperDihedralDefines)[currentProperDihedralDefine].constant, 
+					&(*inputProperDihedralDefines)[currentProperDihedralDefine].np);
+
+				fprintf(ffBondedITP_output, "%s\t%s\t%f\t%f\t%d\n", 
+					(*inputProperDihedralDefines)[currentProperDihedralDefine].defineString, 
+					(*inputProperDihedralDefines)[currentProperDihedralDefine].defineType, 
+					(*inputProperDihedralDefines)[currentProperDihedralDefine].value, 
+					(*inputProperDihedralDefines)[currentProperDihedralDefine].constant * lambda, 
+					(*inputProperDihedralDefines)[currentProperDihedralDefine].np);
+
 				currentProperDihedralDefine++; }
 		}
 	}
 }
 
-void readNonbondedITP (FILE *ffNonbondedITP, TOPOLOGY_BOOL topCurrentPosition, NONBONDED_ATOMTYPES **inputNonbondedAtomtypes, NONBONDED_PARAMS **inputNonbondedParams, int *nNonbondedAtomtypes, int *nNonbondedParams)
+void readNonbondedITP (FILE *ffNonbondedITP, TOPOLOGY_BOOL topCurrentPosition, NONBONDED_ATOMTYPES **inputNonbondedAtomtypes, NONBONDED_PARAMS **inputNonbondedParams, int *nNonbondedAtomtypes, int *nNonbondedParams, FILE *ffBondedITP_output, float lambda)
 {
 	char lineString[2000], rawString[2000];
 
@@ -237,6 +293,9 @@ void readNonbondedITP (FILE *ffNonbondedITP, TOPOLOGY_BOOL topCurrentPosition, N
 	topCurrentPosition.nonbondedParams = 0;
 	(*nNonbondedAtomtypes) = 0;
 	(*nNonbondedParams) = 0;
+
+	int nNonbondedAtomtypes_local = 0, nNonbondedParams_local = 0;
+
 	rewind (ffNonbondedITP);
 
 	// Count the number of entries under each directive
@@ -248,16 +307,13 @@ void readNonbondedITP (FILE *ffNonbondedITP, TOPOLOGY_BOOL topCurrentPosition, N
 				lineString[i] = rawString[i];
 				lineString[i + 1] = '\0'; }
 
-			if ((*nNonbondedAtomtypes) == 0 || (*nNonbondedParams) == 0) {
-				printf("%s\n", lineString); }
-
 			if (strlen (lineString) > 1)
-			{
+			{				
 				if (topCurrentPosition.atomTypes == 1 && lineString[0] == '[') 	{
 					topCurrentPosition.atomTypes = 0; }
 
 				if (topCurrentPosition.atomTypes == 1) {
-					(*nNonbondedAtomtypes)++; }
+					nNonbondedAtomtypes_local++; }
 
 				if (strstr (lineString, "[ atomtypes ]")) {
 					topCurrentPosition.atomTypes = 1; }
@@ -266,7 +322,7 @@ void readNonbondedITP (FILE *ffNonbondedITP, TOPOLOGY_BOOL topCurrentPosition, N
 					topCurrentPosition.nonbondedParams = 0; }
 
 				if (topCurrentPosition.nonbondedParams == 1) {
-					(*nNonbondedParams)++; }
+					nNonbondedParams_local++; }
 
 				if (strstr (lineString, "[ nonbond_params ]")) {
 					topCurrentPosition.nonbondedParams = 1; }
@@ -274,17 +330,20 @@ void readNonbondedITP (FILE *ffNonbondedITP, TOPOLOGY_BOOL topCurrentPosition, N
 		}
 	}
 
-	if ((*nNonbondedAtomtypes) == 0 || (*nNonbondedParams) == 0) {
+	if (nNonbondedAtomtypes_local == 0 || nNonbondedParams_local == 0) {
 		printf("Counting again...\n");
 		fflush (stdout);
-		sleep (1);
+		sleep (10);
 		goto redocounting; }
 
-	printf("\nAllocating %d memory for (*inputNonbondedAtomtypes)\nAllocating %d memory for (*inputNonbondedParams)\n\n", (*nNonbondedAtomtypes), (*nNonbondedParams));
+	printf("\nAllocating %d memory for (*inputNonbondedAtomtypes)\nAllocating %d memory for (*inputNonbondedParams)\n\n", nNonbondedAtomtypes_local, nNonbondedParams_local);
 
 	// Allocating memory
-	(*inputNonbondedAtomtypes) = (NONBONDED_ATOMTYPES *) malloc ((*nNonbondedAtomtypes) * sizeof (NONBONDED_ATOMTYPES));
-	(*inputNonbondedParams) = (NONBONDED_PARAMS *) malloc ((*nNonbondedParams) * sizeof (NONBONDED_PARAMS));
+	(*inputNonbondedAtomtypes) = (NONBONDED_ATOMTYPES *) malloc (nNonbondedAtomtypes_local * sizeof (NONBONDED_ATOMTYPES));
+	(*inputNonbondedParams) = (NONBONDED_PARAMS *) malloc (nNonbondedParams_local * sizeof (NONBONDED_PARAMS));
+
+	(*nNonbondedAtomtypes) = nNonbondedAtomtypes_local;
+	(*nNonbondedParams) = nNonbondedParams_local;
 
 	topCurrentPosition.atomTypes = 0;
 	topCurrentPosition.nonbondedParams = 0;
@@ -302,23 +361,49 @@ void readNonbondedITP (FILE *ffNonbondedITP, TOPOLOGY_BOOL topCurrentPosition, N
 				lineString[i + 1] = '\0'; }
 
 			if (topCurrentPosition.atomTypes == 1 && lineString[0] == '[') 	{
-				topCurrentPosition.atomTypes = 0; }
+				topCurrentPosition.atomTypes = 0;
+				fprintf(ffNonbondedITP_output, "%s\n", lineString); }
 
 			if (topCurrentPosition.atomTypes == 1)
 			{
-				sscanf (lineString, "%s %d %f %f %s %f %f\n", &(*inputNonbondedAtomtypes)[currentAtomtype].name, &(*inputNonbondedAtomtypes)[currentAtomtype].atomicNumber, &(*inputNonbondedAtomtypes)[currentAtomtype].atomicMass, &(*inputNonbondedAtomtypes)[currentAtomtype].atomicCharge, &(*inputNonbondedAtomtypes)[currentAtomtype].ptype, &(*inputNonbondedAtomtypes)[currentAtomtype].c6, &(*inputNonbondedAtomtypes)[currentAtomtype].c12);
+				sscanf (lineString, "%s %d %f %f %s %f %f\n", 
+					&(*inputNonbondedAtomtypes)[currentAtomtype].name, 
+					&(*inputNonbondedAtomtypes)[currentAtomtype].atomicNumber, 
+					&(*inputNonbondedAtomtypes)[currentAtomtype].atomicMass, 
+					&(*inputNonbondedAtomtypes)[currentAtomtype].atomicCharge, 
+					&(*inputNonbondedAtomtypes)[currentAtomtype].ptype, 
+					&(*inputNonbondedAtomtypes)[currentAtomtype].c6, 
+					&(*inputNonbondedAtomtypes)[currentAtomtype].c12);
+
+				fprintf(ffNonbondedITP_output, "%s %d %f %f %s %f %f\n", 
+					(*inputNonbondedAtomtypes)[currentAtomtype].name, 
+					(*inputNonbondedAtomtypes)[currentAtomtype].atomicNumber, 
+					(*inputNonbondedAtomtypes)[currentAtomtype].atomicMass, 
+					(*inputNonbondedAtomtypes)[currentAtomtype].atomicCharge, 
+					(*inputNonbondedAtomtypes)[currentAtomtype].ptype, 
+					(*inputNonbondedAtomtypes)[currentAtomtype].c6 * lambda, 
+					(*inputNonbondedAtomtypes)[currentAtomtype].c12 * lambda);
+
 				currentAtomtype++;
 			}
 
 			if (strstr (lineString, "[ atomtypes ]")) {
-				topCurrentPosition.atomTypes = 1; }
+				topCurrentPosition.atomTypes = 1;
+				fprintf(ffNonbondedITP_output, "%s\n", lineString); }
 
 			if (topCurrentPosition.nonbondedParams == 1 && lineString[0] == '[') {
-				topCurrentPosition.nonbondedParams = 0; }
+				topCurrentPosition.nonbondedParams = 0;
+				fprintf(ffNonbondedITP_output, "%s\n", lineString); }
 
 			if (topCurrentPosition.nonbondedParams == 1)
 			{
-				sscanf (lineString, "%s %s %d %f %f\n", &(*inputNonbondedParams)[currentParams].i, &(*inputNonbondedParams)[currentParams].j, &(*inputNonbondedParams)[currentParams].func, &(*inputNonbondedParams)[currentParams].c6, &(*inputNonbondedParams)[currentParams].c12);
+				sscanf (lineString, "%s %s %d %f %f\n", 
+					&(*inputNonbondedParams)[currentParams].i, 
+					&(*inputNonbondedParams)[currentParams].j, 
+					&(*inputNonbondedParams)[currentParams].func, 
+					&(*inputNonbondedParams)[currentParams].c6, 
+					&(*inputNonbondedParams)[currentParams].c12);
+				
 				currentParams++;
 			}
 
@@ -337,6 +422,8 @@ int main(int argc, char const *argv[])
 	ffBondedITP_output = fopen (argv[4], "w");
 	ffNonbondedITP_output = fopen (argv[5], "w");
 	topolTopITP_output = fopen (argv[6], "w");
+
+	float lambda = atof (argv[7]);
 
 	// Structs to store information from topology file
 	TOPOLOGY_BOOL topCurrentPosition;
@@ -358,11 +445,11 @@ int main(int argc, char const *argv[])
 	NONBONDED_PARAMS *inputNonbondedParams;
 	int nNonbondedAtomtypes, nNonbondedParams;
 
-	inputAtoms = readTopAtoms (topolTopITP, topCurrentPosition, inputAtoms, &nAtoms);
+	inputAtoms = readTopAtoms (topolTopITP, topCurrentPosition, inputAtoms, &nAtoms, topolTopITP_output, lambda);
 
-	readBondedITP (ffBondedITP, topCurrentPosition, &inputBondDefines, &inputAngleDefines, &inputProperDihedralDefines, &inputImproperDihedralDefines, &nBondDefines, &nAngleDefines, &nProperDihedralDefines, &nImproperDihedralDefines);
+	readBondedITP (ffBondedITP, topCurrentPosition, &inputBondDefines, &inputAngleDefines, &inputProperDihedralDefines, &inputImproperDihedralDefines, &nBondDefines, &nAngleDefines, &nProperDihedralDefines, &nImproperDihedralDefines, ffBondedITP_output, lambda);
 
-	readNonbondedITP (ffNonbondedITP, topCurrentPosition, &inputNonbondedAtomtypes, &inputNonbondedParams, &nNonbondedAtomtypes, &nNonbondedParams);
+	readNonbondedITP (ffNonbondedITP, topCurrentPosition, &inputNonbondedAtomtypes, &inputNonbondedParams, &nNonbondedAtomtypes, &nNonbondedParams, ffNonbondedITP_output, lambda);
 
 	free (inputBondDefines);
 	free (inputAngleDefines);
